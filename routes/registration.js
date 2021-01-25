103ebbaea9f673063a333b33bd8853dd106ce256
fox32-pro <soumensamanta.official@gmail.com>
2021-01-25 18:51:16 +0530

    Added Feature of student registration and Table for the results

--- routes/registration.js
+++ routes/registration.js
@@ -6,7 +6,8 @@ router.get('/registration',(req,res, next)=>{
     res.render('registration', {pageTitle : "Exam Result : Registration"});
 });
 router.post('/registration',(req,res, next)=>{
-    database.push({email : bodyParser.emial})
+    database.push({email : req.body.email, marks : req.body.marks});
     res.status(302).redirect('/');
 });
-module.exports = router;
+exports.router = router;
+exports.database = database;
--- routes/result.js
+++ routes/result.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const stData = require('./registration');
 router.get('/result',(req,res, next)=>{
-    
+    res.render('result', {pageTitle : "Exam Result : Result", datas : stData.database});
 })
 module.exports = router;
