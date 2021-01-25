f82d3a0011a4a46d851320d2fa967c7bd1542083
fox32-pro <soumensamanta.official@gmail.com>
2021-01-25 14:36:04 +0530

    Favicon added

--- routes/registration.js
+++ routes/registration.js
@@ -1,9 +1,12 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
+const database = [];
 router.get('/registration',(req,res, next)=>{
     res.render('registration', {pageTitle : "Exam Result : Registration"});
 });
 router.post('/registration',(req,res, next)=>{
+    database.push({email : bodyParser.emial})
     res.status(302).redirect('/');
 });
 module.exports = router;
